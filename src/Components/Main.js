33bc8a6a67cce246613597a82d2db9927d586877
ashleighpresley <ashleighpresley1@gmail.com>
2022-01-31 13:45:32 -0600

    Clean up for photowall app

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,14 +1,11 @@
 import React, { Component } from "react";
-import List from "./List";
 import Title from "./Title";
 
 class Main extends Component {
   render() {
     return (
       <div>
-        <Title title={"To Do"} />
-        <List tasks={["Mow the lawn", "Walk the dog"]} />
-        <List tasks={["Fold laundry", "Wash dishes"]} />
+        <Title title={"Photowall"} />
       </div>
     );
   }
