3f74678e4f84bd14667d7f8f10af3382e991a718
ashleighpresley <ashleighpresley1@gmail.com>
2022-02-04 22:03:21 -0600

    setting up for Firebase

---
+++ src/database/config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from "firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBG-1jKaDuooaLUzRddRjkqCsc-QnVCXUI",
+  authDomain: "photowall-proj-react.firebaseapp.com",
+  databaseURL: "https://photowall-proj-react-default-rtdb.firebaseio.com",
+  projectId: "photowall-proj-react",
+  storageBucket: "photowall-proj-react.appspot.com",
+  messagingSenderId: "477236541021",
+  appId: "1:477236541021:web:d6670336a78c32a76a3fb4",
+  measurementId: "G-JXVYMHLM3B",
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
+export { database };
--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/stylesheet.css";
 import { BrowserRouter } from "react-router-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./redux/reducer";
 import App from "./Components/App";
+import thunk from "redux-thunk";
+import { database } from "./database/config";
 
 //pass in reducer to createStore which defines how data will be updated, can take in multiple reducers but will consilidate them into one root reducer
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
 );
 
 ReactDOM.render(
